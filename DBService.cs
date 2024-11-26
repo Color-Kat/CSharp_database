5515d41445e971a99aed35c341cc698cdbefc5d1
Color-Kat <katik7979@gmail.com>
2024-11-26 11:20:01 +0300

    Read data from db, db service

--- DBService.cs
+++ DBService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Data;
 
 namespace MyDatabase
@@ -57,6 +57,98 @@ namespace MyDatabase
             }
         }
 
+        // ----- Methods for interact with db ----- //
+
+        /**
+         * For queries that return result
+         */
+        public SqlDataReader ExecuteReader(string query)
+        {
+            SqlCommand command = new SqlCommand(query, _connection);
+            return command.ExecuteReader();
+        }
+
+        /**
+         * For queries that do not return a result
+         */
+        public int ExecuteNonQuery(string query)
+        {
+            SqlCommand command = new SqlCommand(query, _connection);
+            return command.ExecuteNonQuery();
+        }
+
+
+        /*
+        // Метод для выполнения SQL-запроса с параметрами
+        public int ExecuteNonQueryWithParams(string query, Dictionary<string, object> parameters)
+        {
+            SqlCommand command = new SqlCommand(query, _connection);
+            foreach (var param in parameters)
+            {
+                command.Parameters.AddWithValue(param.Key, param.Value);
+            }
+            return command.ExecuteNonQuery();
+        }
+
+        // Метод для выполнения BulkCopy (быстрая вставка большого объема данных)
+        public void BulkInsert(DataTable dataTable, string tableName)
+        {
+            using (SqlBulkCopy bulkCopy = new SqlBulkCopy(_connection))
+            {
+                bulkCopy.DestinationTableName = tableName;
+                bulkCopy.BatchSize = 1000; // размер пакета
+                bulkCopy.BulkCopyTimeout = 600; // таймаут
+                bulkCopy.WriteToServer(dataTable);
+            }
+        }
+
+        // Метод для выполнения транзакции (несколько запросов в одном атомарном блоке)
+        public void ExecuteTransaction(List<string> queries)
+        {
+            SqlTransaction transaction = _connection.BeginTransaction();
+
+            try
+            {
+                foreach (var query in queries)
+                {
+                    SqlCommand command = new SqlCommand(query, _connection, transaction);
+                    command.ExecuteNonQuery();
+                }
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+
+        // Асинхронный метод для выполнения запроса на выборку
+        public async Task<SqlDataReader> ExecuteReaderAsync(string query)
+        {
+            SqlCommand command = new SqlCommand(query, _connection);
+            return await command.ExecuteReaderAsync();
+        }
+
+        // Асинхронный метод для выполнения запроса на изменение данных
+        public async Task<int> ExecuteNonQueryAsync(string query)
+        {
+            SqlCommand command = new SqlCommand(query, _connection);
+            return await command.ExecuteNonQueryAsync();
+        }
+
+        // Асинхронный метод для выполнения запроса с параметрами
+        public async Task<int> ExecuteNonQueryWithParamsAsync(string query, Dictionary<string, object> parameters)
+        {
+            SqlCommand command = new SqlCommand(query, _connection);
+            foreach (var param in parameters)
+            {
+                command.Parameters.AddWithValue(param.Key, param.Value);
+            }
+            return await command.ExecuteNonQueryAsync();
+        }
+
+        */
     }
 
 }
--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,15 @@
+using Microsoft.Data.SqlClient;
+
 namespace MyDatabase
 {
     public partial class Form1 : Form
     {
+
+        DBService DBService = new DBService(
+            @"Server=COLORKAT-LAPTOP;Database=MySchool;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;"
+//            @"Server=COLORKAT-LAPTOP;Database=MySchool;Encrypt=True;TrustServerCertificate=True "
+        );
+
         public Form1()
         {
             InitializeComponent();
@@ -14,7 +22,17 @@ namespace MyDatabase
 
         private void uploadCSVButton_Click(object sender, EventArgs e)
         {
+            string query = "SELECT * FROM Students";
+            SqlDataReader reader = DBService.ExecuteReader(query);
 
+            while (reader.Read())
+            {
+                string studentName = reader["student_name"].ToString();
+                int grade = Convert.ToInt32(reader["grade"]);
+                MessageBox.Show($"Student: {studentName}, Grade: {grade}");
+            }
+
+            reader.Close();
         }
 
         private void mergeTablesButton_Click(object sender, EventArgs e)
